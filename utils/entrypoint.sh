6c5f373ecab2ace4174c332d8feb1b75250e6b91
THLIVSQAZ <zgmzzzz18@gmail.com>
2022-09-09 23:18:06 +0800

    fix: zoneminder unable connection local database
    DBI connect('database=zm;host=localhost','zmuser',...) failed: Can't connect to local MySQL server through socket '/var/run/mysqld/mysqld.sock' (2) at /usr/share/perl5/ZoneMinder/Database.pm line 110.
    
    and also mysql default use caching_sha2_password cause connect failed

--- utils/entrypoint.sh
+++ utils/entrypoint.sh
@@ -405,6 +405,7 @@ else
 
     mysql -u root -e "CREATE USER 'zmuser'@'localhost' IDENTIFIED BY 'zmpass';"
     mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'zmuser'@'localhost';"
+    mysql -u root -e "ALTER USER 'zmuser'@'localhost' IDENTIFIED WITH mysql_native_password BY 'zmpass';"
 
     if [ "$(zm_db_exists)" -eq "0" ]; then
         echo " * First run of mysql in the container, creating ZoneMinder dB."
@@ -412,6 +413,8 @@ else
     else
         echo " * ZoneMinder dB already exists, skipping table creation."
     fi
+    
+    sed -i -e "s/ZM_DB_HOST=.*$/ZM_DB_HOST=127.0.0.1/g" $ZMCONF
 fi
 
 # Ensure we shutdown our services cleanly when we are told to stop
